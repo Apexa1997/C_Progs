945abdde097df8f1dcabbd706a38f5fa257df119
Apexa1997 <112808330+Apexa1997@users.noreply.github.com>
2022-09-04 17:36:17 +0530

    Add files via upload
    Area of Square

---
+++ Area_of_square.c
@@ -0,0 +1,11 @@
+// Area of square
+# include <stdio.h>
+int main()
+
+{
+    int side;
+    printf("Enter the value of side: ");        // Value taken from user
+    scanf("%d", &side);
+    printf("Area of square: %d", side * side);    // Calculate are of square
+    return 0;
+}
